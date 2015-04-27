1d2d275cec4ea0d80352cc779b79987bff74dbee
Callum Locke <callum.locke@gmail.com>
2015-04-27 23:49:08 +0100

    init

---
+++ src/foo.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Promise from 'bluebird';
+
+export default class Foo {
+  someProperty = 123;
+  static anotherProperty = 456;
+
+  async doSomething () {
+    await Promise.delay(1000);
+    return 1;
+  }
+}
